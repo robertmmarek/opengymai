0e8b71e9412a2cc624a18cb8cbe3dbf8cc1801fd
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-05-20 10:59:43 +0200

    init

--- CartPole/SimpleNeuralNetworkSolution/SimpleNeuralNetworkCartPole.py
+++ CartPole/SimpleNeuralNetworkSolution/SimpleNeuralNetworkCartPole.py
@@ -1,71 +1,89 @@
-import gym
 import tensorflow as tf
 import numpy as np
+import gym
 
-num_inputs = 4
-num_hidden = 4
-num_outputs = 1
+
+# actions: 0 left, 1 right
+# We will try to predict score of input, where input is both observation and action
+
+# network structure
+input_size = 5
+hidden_size = 4
+output_size = 1
 learning_rate = 0.001
 
-initializer = tf.contrib.layers.variance_scaling_initializer()
+input = tf.placeholder(tf.float32, shape=[None, input_size])
+hid_1 = tf.layers.dense(input, units=hidden_size, activation=tf.nn.relu)
+output = tf.layers.dense(hid_1, units=output_size, activation=None)
 
-X = tf.placeholder(tf.float32, shape=[None, num_inputs])
+y_reward = tf.placeholder(tf.float32, shape=[None, 1])
 
-hidden_layer_1 = tf.layers.dense(X,
-                                 num_hidden,
-                                 activation=tf.nn.relu,
-                                 kernel_initializer=initializer)
-
-hidden_layer_2 = tf.layers.dense(hidden_layer_1,
-                                 num_hidden,
-                                 activation=tf.nn.relu,
-                                 kernel_initializer=initializer)
-
-output_layer = tf.layers.dense(hidden_layer_2,
-                               num_outputs,
-                               activation=tf.nn.sigmoid,
-                               kernel_initializer=initializer)
-
-probabilities = tf.concat(axis=1, values=[output_layer, 1-output_layer])
-action = tf.multinomial(probabilities, num_samples=1)
-
-reward_ph = tf.placeholder(tf.float32, shape=[None, 2])
-loss = tf.reduce_mean(tf.square(reward_ph-output_layer))
+loss = tf.reduce_mean(tf.square(y_reward-output))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-
 train = optimizer.minimize(loss)
+initializer = tf.global_variables_initializer()
 
-init = tf.global_variables_initializer()
 
-epi = 250
-step_limit = 500
-env = gym.make('CartPole-v0')
-avg_steps = []
+def decide_move(sess, obs):
+    left_option = list(obs)+[0.]
+    right_option = list(obs)+[1.]
+
+    out = sess.run(output, feed_dict={input: [left_option, right_option]})
+    return out, np.argmax(out.reshape(2))
+    
+# training params
+
+max_moves_per_game = 50
+training_steps = 100
+policy_gradient_steps = 5
+policy_gradient_discount_rate = 0.3
+
+
+def get_training_data(obs, moves, rewards, estimations):
+    X = []
+    Y_ = []
+    Y = []
+    for i in range(len(rewards)-policy_gradient_steps):
+        sel_obs = obs[i]
+        sel_moves = moves[i]
+        x = list(sel_obs)+[sel_moves]
+        sel_rewards = rewards[i:i+5]
+        y_ = [np.sum([val*np.power(policy_gradient_discount_rate, index) for index, val in enumerate(sel_rewards)])]
+        y = [estimations[i]]
+
+        X.append(x)
+        Y_.append(y_)
+        Y.append(y)
+
+    return X, Y_, Y
 
 with tf.Session() as sess:
-    init.run()
-
-    for i_episode in range(epi):
+    sess.run(initializer)
+    env = gym.make('CartPole-v1')
+    
+    for step in range(training_steps):
         obs = env.reset()
+        env.render()
+        this_game_obs = []
+        this_game_moves = []
+        this_game_rewards = []
+        this_game_estimations = []
+        for _ in range(max_moves_per_game):
+            this_game_obs.append(obs)
+            out_est, move = decide_move(sess, obs)
+            this_game_estimations.append(out_est[move][0])
+            this_game_moves.append(move)
+            print(move)
+            obs = env.step(move)
+            reward = float(obs[1])
+            is_end = obs[2]
+            obs = obs[0]
+            this_game_rewards.append(reward)
 
-        for step in range(step_limit):
-            action_val = action.eval(feed_dict={X: obs.reshape(1, num_inputs)})
-            train_obs = obs
-
-            obs, reward, done, info = env.step(action_val[0][0])
-
-            train_reward = [0., 0.]
-            train_reward[int(action_val)] = reward
-            train_reward[1-int(action_val)] = 1.-reward
-            sess.run(train, feed_dict={X: train_obs.reshape(1, num_inputs),
-                                       reward_ph: np.array([train_reward])})
-
-            if done:
-                avg_steps.append(step)
-                print("Done after {} steps".format(step))
+            if is_end:
                 break
+        print(_)
+        X, Y_, Y = get_training_data(this_game_obs, this_game_moves, this_game_rewards, this_game_estimations)
+        sess.run(train, feed_dict={input: X, y_reward: Y})
 
-    print("After {} episodes steps per game was {}".format(epi,
-                                                           np.mean(avg_steps)))
-    env.close()
 
