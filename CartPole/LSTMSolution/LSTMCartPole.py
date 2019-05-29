267318e979e9ff4503083dc2efbcb68a7a854ab7
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-05-29 13:42:51 +0200

    LSTM rainforced doodle

--- CartPole/LSTMSolution/LSTMCartPole.py
+++ CartPole/LSTMSolution/LSTMCartPole.py
@@ -15,22 +15,32 @@ learning_rate = 0.001
 #define network
 inputs = tf.keras.layers.Input(shape=(None, number_of_inputs))
 lstm_cell = tf.keras.layers.LSTMCell(units=lstm_size)
-lstm_rnn = tf.keras.layers.RNN(lstm_cell, return_sequences=False)
-lstm_outputs = lstm_rnn(inputs)
+lstm_rnn = tf.keras.layers.RNN(lstm_cell, return_sequences=False, return_state=True)
+lstm_state_ph = [tf.placeholder(tf.float32, [None, lstm_size]), tf.placeholder(tf.float32, [None, lstm_size])]
+lstm_outputs = lstm_rnn(inputs, initial_state=lstm_state_ph)
+lstm_states = lstm_outputs[1::]
+lstm_outputs = lstm_outputs[0]
+
 logits = tf.keras.layers.Dense(number_of_logits, activation='sigmoid')(lstm_outputs)
 selection = tf.cast(tf.random.multinomial(logits, 1), tf.float32)
 
-train_selection = tf.placeholder(dtype=tf.float32, shape=[None, 1])
-labels = tf.reshape(tf.stack([1.-train_selection, train_selection], axis=1), shape=[-1, 2])
+labels = tf.reshape(tf.stack([1.-selection, selection], axis=1), shape=[-1, 2])
 loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+gradient_multiplier = tf.placeholder(tf.float32, shape=[1])
 gradients = optimizer.compute_gradients(loss)
+modified_gradients = [(tf.multiply(tup[0], gradient_multiplier), tup[1]) for tup in gradients]
+train = optimizer.apply_gradients(modified_gradients)
 
+zero_states = [[[0.]*lstm_size], [[0]*lstm_size]]
 
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
-    ret = sess.run(selection, feed_dict={inputs: [[[1], [2]]]})
-    print(ret)
+    states = zero_states
+    out, states = sess.run([selection, lstm_states], feed_dict={inputs: [[[1]]], lstm_state_ph[0]: states[0], lstm_state_ph[1]: states[1]})
+    print(states)
+    out, states = sess.run([selection, lstm_states], feed_dict={inputs: [[[1]]], lstm_state_ph[0]: states[0], lstm_state_ph[1]: states[1]})
+    print(states)
     
